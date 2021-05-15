9fce26cc2a91b6ea445295ab8be423c469860672
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-05-16 00:52:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+alert("Trying to learn javascript for the millionth time!");
+alert("Hello there");
+alert("How are you?");
+alert("Have a nice day!");
+
+let x = 5;
+console.log(x);
+
+let y = 10 + 3;
+console.log(y);
+
+let d = 5 - 2;
+console.log(d);
+
+let name = "walt Disney";
+
+console.log(name);
+
+let quote = 'Walt Disney said: "If you can dream it, you can do it!"';
+
+let x;
+
+console.log(x);
+
+x = 5;
+
+console.log(x);
+
+x = null;
+
+console.log(x);
+
+let compare = 3 > 5;
+console.log(compare);
+
+compare = 3 < 5;
+console.log(compare);
+
+
+let fName;
+let lName;
+let age;
+
+fName = prompt("What is your name?");
+lName = prompt("What is your last name?");
+age = prompt("What is your age");
+
+// console.log('Hello, '+ firstName + ' '+ lastName'. You are '+ age +' years old. ');
+// console.log('Hello, '+fName+' '+ lName+'. You are '+age+' years old.')
+
+/
+
+console.log("Hello," + fName + " " + lName + ". You are " +age+ "years old.")
+
+let favouriteDrink = "green tea";
+
+if (favouriteDrink === "coffee") {
+	console.log("I will have coffee.");
+} else if (favouriteDrink === "tea") {
+	console.log("I will have tea");
+} else if (favouriteDrink === "hot chocolate") {
+	console.log("I will have hot chocolate");
+} else {
+	console.log("I will have water");
+}
+
+
+let favouriteDrink = "Hot chocolate";
+
+if (favouriteDrink === "coffee") {
+	console.log("I will have coffee");
+} else if (favouriteDrink === "tea") {
+	console.log("I will have tea");
+}  else if (favouriteDrink === "Hot chocolate") {
+	console.log("I will have hot chocolate");
+}  else {
+	console.log("I will have water");
+}
+
+
+let a = 10;
+let b = 5;
+let c = 2;
+console.log(a + b + c);
+
+ a = a + 5;
+ b = b - 1;
+ c = c / 2;
+console.log(a + b + c);
